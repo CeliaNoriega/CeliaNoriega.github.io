0a4aeca8773fb2d88d6ddb45d3a78ff602890c27
CeliaNoriega <noriegacelia2124@gmail.com>
2021-01-28 19:35:31 -0800

    changed spacing for projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -22,7 +22,7 @@ export default function Album() {
     <React.Fragment>
       <CssBaseline />
       <Container className={classes.cardGrid} maxWidth="md">
-        <Grid container spacing={4}>
+        <Grid container spacing={8}>
           <Grid item xs={12} sm={6} md={4}>
             <Project
              name="Soy Tofu"
